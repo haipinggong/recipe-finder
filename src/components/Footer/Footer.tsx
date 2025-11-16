00f48c9c80693a3b54638e90b3845690340029da
haipinggong <ghaiping@hotmail.com>
2025-11-17 09:49:29 +1100

    Add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,17 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import instagramIcon from "../../assets/images/icon-instagram.svg";
+import blueSkyIcon from "../../assets/images/icon-bluesky.svg";
+import tiktokIcon from "../../assets/images/icon-tiktok.svg";
 
 export const Footer = () => {
-  return <Typography>Footer</Typography>;
+  return (
+    <Box>
+      <Typography>Made with ❤️ and 🥑</Typography>
+      <Box>
+        <img src={instagramIcon} alt="instagram" />
+        <img src={blueSkyIcon} alt="blue sky" />
+        <img src={tiktokIcon} alt="tiktok" />
+      </Box>
+    </Box>
+  );
 };
