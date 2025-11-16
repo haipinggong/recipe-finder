15a532c093be848f83da5320d22178b5b0a09d35
haipinggong <ghaiping@hotmail.com>
2025-11-17 10:02:30 +1100

    Change font size

--- src/theme.ts
+++ src/theme.ts
@@ -7,17 +7,27 @@ declare module "@mui/material/styles" {
   interface PaletteOptions {
     green: PaletteOptions["primary"];
   }
+
+  interface TypographyVariants {
+    fontWeightExtraBold: string | number;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    fontWeightExtraBold?: string | number;
+  }
 }
 
 declare module "@mui/material/Typography" {
   interface TypographyPropsVariantOverrides {
-    fontWeightSemiBold: true;
+    fontWeightExtraBold: true;
   }
 }
 
 let theme = createTheme({
   typography: {
     fontFamily: "Nunito, Nunito Sans, sans-serif",
+    fontWeightExtraBold: 800,
   },
   palette: {
     green: {
@@ -30,14 +40,27 @@ let theme = createTheme({
 theme = createTheme(theme, {
   typography: {
     h1: {
+      fontSize: "4.5rem",
+      lineHeight: "110%",
+      letterSpacing: "-2px",
+      fontWeight: theme.typography.fontWeightExtraBold,
+    },
+    h2: {
+      fontSize: "3rem",
+      lineHeight: "120%",
+      letterSpacing: "-2px",
+      fontWeight: theme.typography.fontWeightExtraBold,
+    },
+    h3: {
       fontSize: "2rem",
-      lineHeight: "100%",
+      lineHeight: "130%",
       letterSpacing: "-1px",
       fontWeight: theme.typography.fontWeightBold,
     },
     body1: {
-      fontSize: "1.125rem",
+      fontSize: "1.25rem",
       lineHeight: "150%",
+      letterSpacing: "-0.4px",
     },
   },
   palette: {
