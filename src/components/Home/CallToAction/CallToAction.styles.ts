b807967ced22d697979feed3f43f27f9297fdb42
haipinggong <ghaiping@hotmail.com>
2025-11-17 16:53:29 +1100

    Add background image for call to action

--- src/components/Home/CallToAction/CallToAction.styles.ts
+++ src/components/Home/CallToAction/CallToAction.styles.ts
@@ -1,4 +1,6 @@
 import { type SxProps, type Theme } from "@mui/material";
+import patternFork from "../../../assets/images/pattern-fork.svg";
+import patternKnife from "../../../assets/images/pattern-knife.svg";
 
 const styles: Record<string, SxProps<Theme>> = {
   container: (theme: Theme) => ({
@@ -10,6 +12,13 @@ const styles: Record<string, SxProps<Theme>> = {
     alignItems: "center",
     gap: 4,
     textAlign: "center",
+    overflow: "hidden",
+    [theme.breakpoints.up("lg")]: {
+      backgroundImage: `url("${patternFork}"), url("${patternKnife}")`,
+      backgroundSize: "auto 90%, auto 90%",
+      backgroundPosition: "left -50px bottom, right -50px bottom",
+      backgroundRepeat: "no-repeat, no-repeat",
+    },
   }),
   content: {
     display: "flex",
