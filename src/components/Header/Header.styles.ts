1b201ef3c93171b02da7f331fff78054e8070528
haipinggong <ghaiping@hotmail.com>
2025-11-22 09:45:27 +1100

    Add drawer

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -4,14 +4,14 @@ export const styles: Record<string, SxProps<Theme>> = {
   container: (theme: Theme) => ({
     padding: 2,
     borderBottom: "1px solid #E0E6E3",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
     [theme.breakpoints.up("sm")]: {
       padding: theme.spacing(4, 4, 2.5, 4),
     },
     [theme.breakpoints.up("md")]: {
       padding: theme.spacing(2.5, 7.5),
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
     },
   }),
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,60 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  useTheme,
+  useMediaQuery,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 import logo from "../../assets/images/logo.svg";
 import { styles } from "./Header.styles";
+import hamburgerMenuIcon from "../../assets/images/icon-hamburger-menu.svg";
+import { useState } from "react";
 
 export const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  console.log(openMenu);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpenMenu(newOpen);
+  };
+
   return (
     <Box component="header" sx={styles.container}>
       <Box component="img" src={logo} alt="logo" />
+      {isMobile && (
+        <IconButton onClick={toggleDrawer(true)}>
+          <Box component="img" src={hamburgerMenuIcon} alt="hamburger menu" />
+        </IconButton>
+      )}
+      <Drawer open={openMenu} onClose={toggleDrawer(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+        >
+          <List>
+            {["Home", "About", "Recipes"].map((text) => (
+              <ListItem key={text}>
+                <ListItemButton
+                  component="a"
+                  href={`/${text.toLowerCase()}`}
+                  onClick={toggleDrawer(false)}
+                >
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
     </Box>
   );
 };
